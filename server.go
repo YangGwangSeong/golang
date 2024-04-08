0abdbac2b9e1f7fea6a345e2210cafb93bb71bc8
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-08 02:59:06 -0700

    feat: Guide/QuickStart/Routing

--- server.go
+++ server.go
@@ -11,5 +11,29 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	// users
+	e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
+	e.PATCH("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
 	e.Logger.Fatal(e.Start(":1323"))
+
 }
+
+func saveUser(c echo.Context) error {
+	return c.String(http.StatusOK, "save User")
+}
+
+func getUser(c echo.Context) error {
+	return c.String(http.StatusOK, "get User")
+}
+
+func updateUser(c echo.Context) error {
+	return c.String(http.StatusOK, "update User")
+}
+
+func deleteUser(c echo.Context) error {
+	return c.String(http.StatusOK, "delete User")
+}
+
